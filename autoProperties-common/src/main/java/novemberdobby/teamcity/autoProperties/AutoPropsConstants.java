d7964e1498ec750d52e5af1998287b086eda4ee5
Unknown <novemberdobby@gmail.com>
2018-01-03 15:45:13 +0000

    -added build started listener (server-side) -added build feature with basic settings jsp

---
+++ autoProperties-common/src/main/java/novemberdobby/teamcity/autoProperties/AutoPropsConstants.java
@@ -0,0 +1,8 @@
+package novemberdobby.teamcity.autoProperties;
+
+public class AutoPropsConstants {
+    public static final String FEATURE_TYPE_ID = "auto_properties_set";
+    public static final String FEATURE_DISPLAY_NAME = "Auto Properties";
+    public static final String FEATURE_PARAMS_DESC = "PLACEHOLDER PLACEHOLDER";
+    public static final String FEATURE_SETTINGS_JSP = "settings.jsp";
+}
